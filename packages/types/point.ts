b1d2acbd63cecabd47bdcd3cb2f6c765d7cf3cb4
Tanmoy <89120102+tempewda@users.noreply.github.com>
2023-07-04 23:43:13 +0530

    ✨ add comments in point.ts

--- packages/types/point.ts
+++ packages/types/point.ts
@@ -1,5 +1,11 @@
+/*
+ * represent point coordinates or positions
+ */
 export type Point = { x: number; y: number };
 
+/*
+ * array that represents the four cardinal directions in a 2D grid: right, down, left, and up, respectively
+ */
 export const around4 = [
   { x: 1, y: 0 },
   { x: 0, y: -1 },
@@ -7,4 +13,7 @@ export const around4 = [
   { x: 0, y: 1 },
 ] as const;
 
+/*
+ * returns true if 2 points are equal
+ */
 export const pointEquals = (a: Point, b: Point) => a.x === b.x && a.y === b.y;
