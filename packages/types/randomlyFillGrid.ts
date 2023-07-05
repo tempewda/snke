0db774db6a53b2af472fc1ea416468e2daeaa70e
Tanmoy <89120102+tempewda@users.noreply.github.com>
2023-07-05 10:26:32 +0530

    Update randomlyFillGrid.ts

--- packages/types/randomlyFillGrid.ts
+++ packages/types/randomlyFillGrid.ts
@@ -1,8 +1,18 @@
 import { Grid, Color, setColor, setColorEmpty } from "./grid";
 
+/*
+ * defaultRand generates a random integer between a and b (inclusive). 
+ * It uses the Math.random() function to generate a random number between 0 and 1, 
+ * multiplies it by the range (b - a + 1), and adds a to ensure the result is within the desired range. 
+ */
 const defaultRand = (a: number, b: number) =>
   Math.floor(Math.random() * (b - a + 1)) + a;
 
+/*
+ * defaultRand generates a random integer between a and b (inclusive). 
+ * It uses the Math.random() function to generate a random number between 0 and 1, 
+ * multiplies it by the range (b - a + 1), and adds a to ensure the result is within the desired range. 
+ */
 export const randomlyFillGrid = (
   grid: Grid,
   {
